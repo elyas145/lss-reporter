d09b4cc4a7636fe6fa161b0578fcc178c019a8ab
Elyas <Elyas@192.168.2.10>
2016-08-15 11:16:26 -0400

    updater implementation complete. not working, neet to debug.

--- Updater/Updater/src/main/java/LssReporter/Updater/MainController.java
+++ Updater/Updater/src/main/java/LssReporter/Updater/MainController.java
@@ -2,7 +2,10 @@ package LssReporter.Updater;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Properties;
 import java.util.ResourceBundle;
@@ -34,6 +37,7 @@ public class MainController implements Initializable {
 	ProgressBar prgsProgress;
 	@FXML
 	Button btnStart;
+	public Integer cachedVersion = -1;
 
 	public void initialize(URL location, ResourceBundle resources) {
 		prgsProgress.setProgress(-1);
@@ -52,20 +56,23 @@ public class MainController implements Initializable {
 					props.put("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
 
 					SMTPAuthenticator auth = new SMTPAuthenticator();
-					
+
 					Session session = Session.getDefaultInstance(props);
 					Store store = session.getStore("imap");
 					PasswordAuthentication authentication = auth.getPasswordAuthentication();
 					store.connect(authentication.getUserName(), authentication.getPassword());
-					
+
 					Folder folderInbox = store.getFolder("INBOX");
 					folderInbox.open(Folder.READ_ONLY);
 
 					SearchTerm searchCondition = new SearchTerm() {
+						private static final long serialVersionUID = -8159023234781467178L;
+
 						@Override
 						public boolean match(Message message) {
 							try {
-								if (message.getSubject().contains("update")) {
+								if (message.getSubject().contains("update")
+										&& message.getFrom()[0].toString().contains("elyas.syoufi@gmail.com")) {
 									return true;
 								}
 							} catch (MessagingException ex) {
@@ -76,16 +83,64 @@ public class MainController implements Initializable {
 					};
 
 					Message[] foundMessages = folderInbox.search(searchCondition);
+					List<Update> updates = new ArrayList<Update>();
+					onProgressUpdate(new ProgressUpdate("Found " + foundMessages.length + " potential update(s).", -1));
 
 					for (int i = 0; i < foundMessages.length; i++) {
 						Message message = foundMessages[i];
 						String subject = message.getSubject();
 						System.out.println("Found message #" + i + ": " + subject);
+						updates.add(new Update(message));
 					}
 
+					// sort the updates list.
+					updates.sort(new Comparator<Update>() {
+
+						public int compare(Update o1, Update o2) {
+							if (o1.getUpdateNumber() < o2.getUpdateNumber()) {
+								return -1;
+							}
+							if (o1.getUpdateNumber() > o2.getUpdateNumber()) {
+								return 1;
+							}
+							return 0;
+						}
+					});
+
+					int currentVersion = getCurrentUpdateVersion();
+					for (int i = 0; i < updates.size(); i++) {
+						Update cUpdate = updates.get(i);
+						if (currentVersion < cUpdate.getUpdateNumber()) {
+							onProgressUpdate(
+									new ProgressUpdate("Initializing update " + (i + 1) + " of " + updates.size(),
+											i + 1 / updates.size()));
+							try {
+								cUpdate.initUpdate();
+								onProgressUpdate(
+										new ProgressUpdate("Applying update " + (i + 1) + " of " + updates.size(),
+												i + 1 / updates.size()));
+								cUpdate.applyUpdate();
+								currentVersion = cUpdate.getUpdateNumber();
+							} catch (Exception e) {
+								addException("Error upplying update " + cUpdate.getUpdateNumber(), e);
+							}
+						}
+					}
 					// disconnect
 					folderInbox.close(false);
 					store.close();
+
+					// update the version.
+					if (currentVersion != -1) {
+						InputStream is = MainController.class.getResourceAsStream("/secret.json");
+						String jsonTxt = IOUtils.toString(is);
+						JSONObject obj = new JSONObject(jsonTxt);
+						obj.put("version", "" + currentVersion);
+						try (PrintWriter out = new PrintWriter(
+								MainController.class.getResource("/secret.json").getPath())) {
+							out.println(obj.toString(4));
+						}
+					}
 					return "Successfully installed updates.";
 				} catch (Exception exception) {
 					addException("an unknown error has occured.", exception);
@@ -95,14 +150,15 @@ public class MainController implements Initializable {
 
 			@Override
 			public void updateProgress(ProgressUpdate param) {
-				// TODO Auto-generated method stub
+				lblStatus.setText(param.getMessage());
+				prgsProgress.setProgress(param.getProgress());
 
 			}
 
 			@Override
 			public void onFinish(String param, List<Exception> exceptions) {
-				if(!exceptions.isEmpty()){
-					for(Exception exception : exceptions){
+				if (!exceptions.isEmpty()) {
+					for (Exception exception : exceptions) {
 						exception.printStackTrace();
 					}
 				}
@@ -120,6 +176,28 @@ public class MainController implements Initializable {
 
 	}
 
+	private int getCurrentUpdateVersion() {
+		if (cachedVersion == -1) {
+
+			InputStream is = MainController.class.getResourceAsStream("/secret.json");
+			if (is == null) {
+				return -1;
+			}
+
+			String jsonTxt;
+			try {
+				jsonTxt = IOUtils.toString(is);
+				JSONObject obj = new JSONObject(jsonTxt);
+				cachedVersion = Integer.valueOf(obj.optString("version"));
+
+			} catch (IOException e) {
+				e.printStackTrace();
+				return -1;
+			}
+		}
+		return cachedVersion;
+	}
+
 	private class SMTPAuthenticator extends javax.mail.Authenticator {
 		public PasswordAuthentication getPasswordAuthentication() {
 			InputStream is = MainController.class.getResourceAsStream("/secret.json");
@@ -130,6 +208,7 @@ public class MainController implements Initializable {
 			try {
 				jsonTxt = IOUtils.toString(is);
 				JSONObject obj = new JSONObject(jsonTxt);
+				cachedVersion = Integer.valueOf(obj.optString("version"));
 				return new PasswordAuthentication(obj.optString("user"), obj.optString("password"));
 			} catch (IOException e) {
 				e.printStackTrace();
---
+++ Updater/Updater/src/main/java/LssReporter/Updater/Update.java
@@ -0,0 +1,237 @@
+package LssReporter.Updater;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Part;
+
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Update {
+	private Message message;
+	private String subject;
+	private int updateNumber;
+	private List<File> attachments;
+	private List<UpdateInstruction> instructions;
+
+	public static enum Type {
+		ADD, DELETE, REPLACE
+	};
+
+	/**
+	 * this update is dependent on the message passed in. the properties should
+	 * not be set outside this class.
+	 * 
+	 * @param message
+	 *            the message with the update instructions.
+	 * @throws MessagingException
+	 *             if unable to read the message subject
+	 * @throws NumberFormatException
+	 *             if the message subject is in the wrong format.
+	 */
+	public Update(Message message) throws MessagingException, NumberFormatException {
+		this.message = message;
+		this.subject = message.getSubject();
+		this.updateNumber = Integer.valueOf(subject.trim().substring(subject.indexOf(' '), subject.length()).trim());
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public int getUpdateNumber() {
+		return updateNumber;
+	}
+
+	/**
+	 * applies this update. initUpdate() must to be called first to download
+	 * necessary data from message.
+	 * 
+	 * @throws IOException
+	 */
+	public void applyUpdate() throws IOException {
+		if (instructions == null) {
+			throw new NullPointerException("Update not initialized properly. instructions missing.");
+		}
+		for (UpdateInstruction instruction : instructions) {
+			switch (instruction.type) {
+			case ADD:
+				addFile(instruction.path, instruction.fileName);
+				break;
+			case DELETE:
+				deleteFile(instruction.path, instruction.fileName);
+				break;
+			case REPLACE:
+				addFile(instruction.path, instruction.fileName);
+			default:
+				break;
+			}
+		}
+		// delete temp files.
+		for (File file : attachments) {
+			file.delete();
+		}
+	}
+
+	private void deleteFile(String path, String fileName) throws FileNotFoundException {
+		File file = new File(path + fileName);
+		if (file.exists()) {
+			file.delete();
+		} else {
+			throw new FileNotFoundException("the file \"" + fileName + "\" could not be found.");
+		}
+
+	}
+
+	private void addFile(String path, String fileName) throws IOException {
+		File file = new File(path + fileName);
+		file.getParentFile().mkdirs();
+		for (File file2 : attachments) {
+			if (file2.getName().trim().toLowerCase().equals(fileName.trim().toLowerCase())) {
+				Files.copy(file2.toPath(), file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+			}
+		}
+	}
+
+	/**
+	 * initializes this object from the message. only call this method if the
+	 * update is going to happen.calling this method on all potential updates
+	 * will waste a lot of time.
+	 * 
+	 * @throws MessagingException
+	 *             if the message cannot be read.
+	 * @throws IOException
+	 *             if attachments cannot be downloaded.
+	 */
+
+	public void initUpdate() throws MessagingException, IOException {
+		String inst = getText(message);
+		if (textIsHtml) {
+			System.out.println("text is html :(");
+		}
+		attachments = getAttachments(message);
+		parseInstructions(inst);
+	}
+
+	private void parseInstructions(String inst) {
+		instructions = new ArrayList<Update.UpdateInstruction>();
+		JSONArray array = new JSONArray(inst);
+		for (int i = 0; i < array.length(); i++) {
+			JSONObject instruction = array.getJSONObject(i);
+			instructions.add(new UpdateInstruction(instruction));
+		}
+	}
+
+	private List<File> getAttachments(Message message) throws IOException, MessagingException {
+		List<File> attachments = new ArrayList<File>();
+		Multipart multipart = (Multipart) message.getContent();
+		// System.out.println(multipart.getCount());
+
+		for (int i = 0; i < multipart.getCount(); i++) {
+			BodyPart bodyPart = multipart.getBodyPart(i);
+			if (!Part.ATTACHMENT.equalsIgnoreCase(bodyPart.getDisposition())
+					&& !StringUtils.isNotBlank(bodyPart.getFileName())) {
+				continue; // dealing with attachments only
+			}
+			InputStream is = bodyPart.getInputStream();
+			File f = new File(Update.class.getResource("/").getPath() + "/tmp/" + bodyPart.getFileName());
+			System.out.println("file: " + f.getAbsolutePath());
+			f.getParentFile().mkdirs();
+			f.createNewFile();
+			FileOutputStream fos = new FileOutputStream(f);
+			byte[] buf = new byte[4096];
+			int bytesRead;
+			while ((bytesRead = is.read(buf)) != -1) {
+				fos.write(buf, 0, bytesRead);
+			}
+			fos.close();
+
+			attachments.add(f);
+		}
+		return attachments;
+	}
+
+	private boolean textIsHtml = false;
+
+	/**
+	 * Return the primary text content of the message.
+	 */
+	private String getText(Part p) throws MessagingException, IOException {
+		if (p.isMimeType("text/*")) {
+			String s = (String) p.getContent();
+			textIsHtml = p.isMimeType("text/html");
+			return s;
+		}
+
+		if (p.isMimeType("multipart/alternative")) {
+			// prefer html text over plain text
+			Multipart mp = (Multipart) p.getContent();
+			String text = null;
+			for (int i = 0; i < mp.getCount(); i++) {
+				Part bp = mp.getBodyPart(i);
+				if (bp.isMimeType("text/plain")) {
+					if (text == null) {
+						text = getText(bp);
+						return text;
+					}
+
+				} else if (bp.isMimeType("text/html")) {
+					continue;
+				} else {
+					return getText(bp);
+				}
+			}
+			return text;
+		} else if (p.isMimeType("multipart/*")) {
+			Multipart mp = (Multipart) p.getContent();
+			for (int i = 0; i < mp.getCount(); i++) {
+				String s = getText(mp.getBodyPart(i));
+				if (s != null)
+					return s;
+			}
+		}
+		return null;
+	}
+
+	private class UpdateInstruction {
+		public UpdateInstruction(JSONObject instruction) {
+			String type = instruction.getString("operation");
+			switch (type.trim().toLowerCase()) {
+			case "add":
+				this.type = Type.ADD;
+				break;
+			case "delete":
+				this.type = Type.DELETE;
+				break;
+			case "replace":
+				this.type = Type.REPLACE;
+				break;
+			default:
+				break;
+			}
+
+			this.path = instruction.getString("path");
+			this.fileName = instruction.getString("file");
+		}
+
+		protected Type type;
+		protected String path;
+		protected String fileName;
+
+	}
+}
